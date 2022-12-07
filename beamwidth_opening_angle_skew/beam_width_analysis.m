8a21960da4efcc6ba80350956e0215d5df6ae79b
unknown <morgan.roberts.18@ucl.ac.uk>
2022-12-07 18:43:45 +0000

    finished beam_width_analysis

--- beamwidth_opening_angle_skew/beam_width_analysis.m
+++ beamwidth_opening_angle_skew/beam_width_analysis.m
@@ -4,148 +4,140 @@ clearvars
 [~, data_dir]    = getRepoDataPath();
 scan_data_folder = [data_dir, filesep, 'field_scans'];
 
-
 tic;
 disp('Loading data ...');
-filename       = 'isolated_reprojected_beam_data_ch1';
+filename       = 'isolated_reprojected_beam_data_probes_AEF';
 input_filename = [scan_data_folder, filesep, filename, '.mat'];
 load(input_filename, 'beam_data', 'freqs', 'c_water', 'dx', 'Ny', 'Nx', 'Fs', 'dt', 'z_proj', 'z_targ', 'Nfreqs');
 disp(['Completed in ', num2str(toc), ' s']);
 fprintf('\n');
-
-% Extract the plane at z = 110mm, the middle of the ring array
-mid_plane = beam_data(:,:,:,end);
-
+%%
 % Calculate x and y position vectors
 x_pos = 0:dx:(Nx-1) * dx;
 x_pos = x_pos - x_pos( round(Nx / 2) );
 y_pos = 0:dx:(Ny-1) * dx;
 y_pos = y_pos - y_pos( round(Ny / 2) );
 
-%%
-% Extract the plane for the largest frequency component
-[~,I]        = max(mid_plane, [], 'all');
-[~, ~, i_f]  = ind2sub(size(mid_plane), I);
-mid_plane_fc = mid_plane(:,:,i_f);
+% Setup loop parameters
+Nel = size(beam_data, 5);
+zdx = 5;
 
-% Threshold the plane [dB rel to max]
-mid_plane_dB = 20 * log10(mid_plane_fc / max(mid_plane_fc(:)));
+% Setup storage
+ele_resp    = zeros(Ny, Nfreqs, Nel);
+ele_resp_fc = zeros(Ny, Nel);
+ycs         = zeros(1, Nel);
+beamwidth   = zeros(1, Nel);
+
+% Threshold for defining the edge of the beam
 thresh = -6;
-mid_plane_thresh = mid_plane_dB;
-mid_plane_thresh(mid_plane_thresh < thresh) = NaN;
-mask = ~isnan(mid_plane_thresh);
 
-% Compute the location of the weighted centroid of the thresholded plane
-props = regionprops(mask, mid_plane_thresh, 'WeightedCentroid');
-i_x   = round(props.WeightedCentroid(1));
-i_y   = round(props.WeightedCentroid(2));
+% Loop over each element
+for edx = 1:Nel
+    % Extract the data plane at z=110mm for the current element
+    % volume_data has dims (Ny, Nx, Nfreqs)
+    volume_data = squeeze( beam_data(:, :, :, zdx, edx) );
 
-figure;
-h = imagesc(mid_plane_thresh);
-set(h, 'AlphaData', mask);
-colorbar
-colormap(getBatlow);
-axis image
-xline(i_x, 'k--');
-yline(i_y, 'k--');
+    % Find the centroid and edges of the -6dB bounding box in 3D
+    [X, ~, F] = locateThresholdRegion(volume_data, thresh);
+
+    % Extract and store the elevational response spectrum at the centroid
+    % This has dims (Ny, Nf)
+    ele_resp(:, :, edx) = squeeze( volume_data(:, X.ic, :) );
+
+    % Extract and store the elevational cross section at f_c
+    % This has dims (1, Ny)
+    ele_xsec = volume_data(:, X.ic, F.ic);
+    ele_resp_fc(:, edx) = ele_xsec;
+
+    % Compute the beamwidth
+    beamwidth(edx) = fwhm(ele_xsec, dx, 0);
+  
+end
+
+% % Correct for the beam skew by shifting 
+% figure;
+% plot(y_pos, squeeze(ele_lines(100,:,:)) )
+% 
+% Npad = 15;
+% pad = NaN * ones(Nfreqs, Npad, Nel);
+% ele_lines_pad = cat(2, pad, ele_lines, pad);
+% ele_lines_shift = NaN * ones(size(ele_lines_pad));
+% i_centre = round(Ny / 2);
+% for edx = 1:Nel
+%     i_shift = i_centre - ycs(edx);
+%     ele_lines_shift(:,:,edx) = circshift(ele_lines_pad(:,:,edx), [0, i_shift]);
+% end
+% 
+% % Remove the NaN padding
+% ele_lines_shift = ele_lines_shift(:, Npad+1:end-Npad, :);
+% 
+% figure;
+% plot( squeeze(ele_lines_shift(100,:,:)) )
+% 
+% ele_lines = ele_lines_shift;
+
+% Calculate the mean elevational response and the standard deviation
+mean_bw = mean(beamwidth);
+std_bw  = std(beamwidth);
+
+% Calculate the mean elevational response, convert to dB and compute
+% standard deviation
+mean_resp = mean(ele_resp, 3);
+max_val   = max(mean_resp(:));
+mean_dB   = 20 * log10(mean_resp / max_val);
+std_resp  = std(ele_resp, 0, 3);
+std_pc    = 1e2 * std_resp / max_val;
 
 % ------------------------------------------------------------------------
-figure;
-subplot(3, 1, 1);
-h = imagesc(x_pos*1e3, y_pos*1e3, mid_plane_thresh);
-set(h, 'AlphaData', mask);
-c = colorbar;
-colormap(getBatlow);
-axis image
-xline(x_pos(i_x)*1e3, 'k--');
-yline(y_pos(i_y)*1e3, 'k--');
-xlabel('x-position [mm]');
-xlabel('y-position [mm]');
-ylabel(c, 'Pressure [dB]');
-title('z=110mm at f=1.2MHz');
+% Plot the mean elevational response and the standard deviation
 
-subplot(3, 1, 2);
-hold on;
-plot(x_pos*1e3, squeeze( mid_plane_dB(i_y,:) ), 'k' );
-title('Lateral Plane');
-xlabel('x-position [mm]');
-ylabel('Pressure [dB]');
-
-subplot(3, 1, 3);
-hold on;
-plot(y_pos, squeeze( mid_plane_dB(:,i_x) ), 'k' );
-title('Elevation Plane');
-xlabel('y-position [mm]');
-ylabel('Pressure [dB]');
-
-%%
-% Use the centroids to extract the elevational and lateral planes
-
-ele_beam = squeeze(mid_plane(:,i_x,:))';
-lat_beam = squeeze(mid_plane(i_y,:,:))';
-
-% ele_beam = ele_beam.^2;
-% lat_beam = lat_beam.^2;
-
-% Heuristic smoothing for now until mean data is available across all 
-k = 7;
-ele_beam = movmean(ele_beam, k, 1);
-lat_beam = movmean(lat_beam, k, 1);
-
-ele_beam_dB = 20 * log10(ele_beam / max(ele_beam(:)));
-lat_beam_dB = 20 * log10(lat_beam / max(lat_beam(:)));
-
-ele_beam_mask = ele_beam_dB > thresh;
-lat_beam_mask = lat_beam_dB > thresh;
-
-ele_y_sum = sum(ele_beam_mask, 1);
-ele_L     = find(ele_y_sum, 1, 'first');
-ele_R     = find(ele_y_sum, 1, 'last');
-ele_f_sum = sum(ele_beam_mask, 2);
-ele_T     = find(ele_f_sum, 1, 'first');
-ele_B     = find(ele_f_sum, 1, 'last');
-
-lat_y_sum = sum(lat_beam_mask, 1);
-lat_L     = find(lat_y_sum, 1, 'first');
-lat_R     = find(lat_y_sum, 1, 'last');
-lat_f_sum = sum(lat_beam_mask, 2);
-lat_T     = find(lat_f_sum, 1, 'first');
-lat_B     = find(lat_f_sum, 1, 'last');
+% Find the -6dB edges of the mean elevational response for plotting
+[~, Y, ~] = locateThresholdRegion( permute( mean_resp, [1, 2, 3] ), thresh );
+plot_thresh = -30;
 
 figure;
 subplot(1, 2, 1);
-h = imagesc(y_pos*1e3, freqs*1e-6, ele_beam_dB, [-20, 0]);
-% set(h, 'AlphaData', ele_beam_mask);
-% contourf(y_pos*1e3, freqs*1e-6, ele_beam_dB, -10:1:0);
+hold on;
+imagesc(y_pos*1e3, freqs*1e-6, mean_dB', [plot_thresh, 0]);
+c = colorbar;
 set(gca, 'YDir', 'normal');
+colormap(getBatlow);
+axis square
 ylabel('Frequency [MHz]');
 xlabel('y-position [mm]');
-title('Elevation Plane (Squared - TxRx)');
-c = colorbar;
-colormap(getBatlow);
 ylabel(c, 'Pressure [dB]');
-xline( y_pos(ele_L)*1e3, 'k--' );
-xline( y_pos(ele_R)*1e3, 'k--' );
-yline( freqs(ele_T)*1e-6, 'k--' );
-yline( freqs(ele_B)*1e-6, 'k--' );
-axis square
+xline(y_pos(Y.imin)*1e3, 'k--', 'linewidth', 1.5);
+xline(y_pos(Y.imax)*1e3, 'k--', 'linewidth', 1.5);
+xlim( 1e3 * y_pos([1, end]) );
+ylim( 1e-6 * freqs([1, end]) );
 
 subplot(1, 2, 2);
-h = imagesc(x_pos*1e3, freqs*1e-6, lat_beam_dB, [-20, 0]);
-% set(h, 'AlphaData', lat_beam_mask);
-% contourf(x_pos*1e3, freqs*1e-6, lat_beam_dB, -10:1:0);
-set(gca, 'YDir', 'normal');
-ylabel('Frequency [MHz]');
-xlabel('x-position [mm]');
-title('Lateral Plane');
+imagesc(y_pos*1e3, freqs*1e-6, std_pc');
 c = colorbar;
+set(gca, 'YDir', 'normal');
 colormap(getBatlow);
-ylabel(c, 'Pressure [dB]');
-xline( x_pos(lat_L)*1e3, 'k--' );
-xline( x_pos(lat_R)*1e3, 'k--' );
-yline( freqs(lat_T)*1e-6, 'k--' );
-yline( freqs(lat_B)*1e-6, 'k--' );
 axis square
+ylabel('Frequency [MHz]');
+xlabel('y-position [mm]');
+ylabel(c, 'Standard Deviation [%]');
+xlim( 1e3 * y_pos([1, end]) );
+ylim( 1e-6 * freqs([1, end]) );
 
+% ------------------------------------------------------------------------
+% Plot the beamwidth histogram
 
+face_colour = [175, 238, 238]/255;
 
+figure;
+h2 = histogram(beamwidth*1e3, 10);
+set(h2,'facecolor',face_colour);
+hold on
+f = histfit(beamwidth*1e3);
+delete(f(1));
+set(f(2),'color','k');
+xlabel('Beamwidth [mm]');
+ylabel('Counts');
+xlim(1e3*mean_bw + [-1.5, 1.5]);
+leg = ['Gaussian fit', newline, '\mu = ', num2str(1e3*mean_bw, 3),...
+       ' mm', newline, '\sigma = ', num2str(std_bw*1e3, 3), ' mm'];
+legend([f(2)], {leg}, 'location', 'northoutside', 'numcolumns', 2);
